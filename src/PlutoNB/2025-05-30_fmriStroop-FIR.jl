67cee1b16624edc2aa2230dfa4733acfca2ace66
behinger (s-ccs 001) <benedikt.ehinger@vis.uni-stuttgart.de>
2025-06-06 09:58:14 +0200

    newest FIR notebooks

--- src/PlutoNB/2025-05-30_fmriStroop-FIR.jl
+++ src/PlutoNB/2025-05-30_fmriStroop-FIR.jl
@@ -14,6 +14,9 @@ end
 # ╔═╡ a6fb95f6-1ed0-480f-afb5-1cbf6ff6bb3c
 Pkg.add("DSP")
 
+# ╔═╡ 387e6752-5de1-4d46-931f-b5a2af62c0f9
+Pkg.add("CUDA")
+
 # ╔═╡ 6e6a5571-28f3-4e58-9118-24d015e8c87a
 using UnfoldBIDS
 
@@ -52,8 +55,13 @@ using HypothesisTests
 # ╔═╡ 0ade7e1f-c3ab-46af-af23-fb4fbe939737
 using NaNStatistics
 
-# ╔═╡ 387e6752-5de1-4d46-931f-b5a2af62c0f9
-CondaPkg
+# ╔═╡ c1c2ee66-bec6-4a6f-ae0f-df168c9ac22a
+using CUDA
+
+# ╔═╡ 76f5c899-26a2-4bf3-b390-1f76ccf594d4
+md"""
+# FIR Version!
+"""
 
 # ╔═╡ 8021cedb-64e8-4fe8-a609-8ee08eb0b503
 begin
@@ -153,7 +161,11 @@ end
 # ╔═╡ 9b465222-edf9-4b05-980c-60466ad0c912
 ni = do_read_nifti(path_ix_nifti)
 
-# ╔═╡ 934cae17-fbc7-436f-bca5-8a9add83192d
+# ╔═╡ 56df2f62-6600-433b-ab3f-5d4d2949a0a3
+_evts2 = do_read_tsv(1)
+
+
+# ╔═╡ 59e19266-7cba-47d9-a7f1-3e9776519f9e
 
 
 # ╔═╡ b77e5a08-eec9-4984-a971-68234137b32b
@@ -170,88 +182,32 @@ begin
 	end
 end
 
-# ╔═╡ 43c679a1-af3d-4193-8865-fe65f8c5f7f4
-# ------------------------------------------------------------------------------------------
-"""
-    winsorize(
-        x::AbstractVector; 
-        probs::Union{Tuple{Real, Real}, Nothing} = nothing,
-        cutpoints::Union{Tuple{Real, Real}, Nothing} = nothing,
-        replace::Symbol = :missing
-        verbose::Bool=false
-    )
-
-# Arguments
-- `x::AbstractVector`: a vector of values
-
-# Keywords
-- `probs::Union{Tuple{Real, Real}, Nothing}`: A vector of probabilities that can be used instead of cutpoints
-- `cutpoints::Union{Tuple{Real, Real}, Nothing}`: Cutpoints under and above which are defined outliers. Default is (median - five times interquartile range, median + five times interquartile range). Compared to bottom and top percentile, this takes into account the whole distribution of the vector
-- `replace_value::Tuple`:  Values by which outliers are replaced. Default to cutpoints. A frequent alternative is missing. 
-- `IQR::Real`: when inferring cutpoints what is the multiplier from the median for the interquartile range. (median ± IQR * (q75-q25))
-- `verbose::Bool`: printing level
-
-# Returns
-- `AbstractVector`: A vector the size of x with substituted values 
-
-# Examples
-- See tests
-
-This code is based on Matthieu Gomez winsorize function in the `statar` R package 
-"""
-function winsorize(x::AbstractVector{T}; 
-    probs::Union{Tuple{Real, Real}, Nothing} = nothing,
-    cutpoints::Union{Tuple{Union{T, Real}, Union{T, Real}}, Nothing} = nothing,
-    replace_value::Union{Tuple{Union{T, Real}, Union{T, Real}}, Tuple{Missing, Missing}, Nothing, Missing} = nothing,
-    IQR::Real=3,
-    verbose::Bool=false
-    ) where T
-
-    if !isnothing(probs)
-        lower_percentile = max(minimum(probs), 0)
-        upper_percentile = min(maximum(probs), 1)
-        (lower_percentile<0 || upper_percentile>1) && @error "bad probability input"
-        verbose && any(ismissing, x) && (@info "Some missing data skipped in winsorizing")
-        verbose && !isnothing(cutpoints) && (@info "input cutpoints ignored ... using probabilities")
-
-        cut_lo = (lower_percentile==0) ? minimum(skipmissing(x)) : quantile(skipmissing(x), lower_percentile)
-        cut_hi = (upper_percentile==1) ? maximum(skipmissing(x)) : quantile(skipmissing(x), upper_percentile)
-        cutpoints = (cut_lo, cut_hi)
-        
-    elseif isnothing(cutpoints)
-        verbose && any(ismissing, x) && (@info "Some missing data skipped in winsorizing")
-        l = quantile(skipmissing(x), [0.25, 0.50, 0.75])
-        cutpoints = (l[2] - IQR * (l[3]-l[1]), l[2] + IQR * (l[3]-l[1]) )
-        verbose && @info "Inferred cutpoints are ... $cutpoints (using interquartile range x $IQR from median)"
-    end
-
-    if isnothing(replace_value) # default to  cutpoints
-        replace_value = (minimum(cutpoints), maximum(cutpoints))
-        replace_value = convert.(Union{T, eltype(replace_value)}, replace_value)
-    elseif ismissing(replace_value)
-        replace_value = (missing, missing)
-    end
-
-    if any(ismissing.(replace_value))
-        y = Vector{Union{T, Missing}}(x)  # Make a copy of x that can also store missing values
-    else
-        y = Vector{Union{T, eltype(replace_value)}}(x) # TODO could be faster using views here ...
-    end
-    
-    y[findall(skipmissing(x .< cutpoints[1]))] .= replace_value[1];
-    y[findall(skipmissing(x .> cutpoints[2]))] .= replace_value[2];
-
-    return y
+# ╔═╡ f9d45bce-626b-4666-8c19-da033ddcc504
+# ╠═╡ disabled = true
+#=╠═╡
+begin
+	colormap = to_colormap(:plasma)
+colormap[1] = RGBAf(0,0,0,0)
+f3,ax3,h3 = volume(1 .- log10.(roi4_with_nlin_p),algorithm=:absorption,colormap=colormap,absorption=50)
+	Colorbar(f3[1,2],h3)
+	f3
 end
+  ╠═╡ =#
 
-# ╔═╡ f619dbf7-a6b9-45b1-af79-6681aa8d9f67
-
+# ╔═╡ 7945f3af-c491-4eee-a461-54d7314ad0b6
+const AoG = UnfoldMakie.AlgebraOfGraphics
 
 # ╔═╡ 9f896b11-f7ef-44ed-b9e1-191783ef86a0
 
 
 # ╔═╡ ccd29bf5-bd65-4ae9-a3a2-b82e76c87096
 WGLMakie.Page()
+
+# ╔═╡ 2db9cac5-efc8-4955-a42c-ff5e193ad9fc
+#=╠═╡
+contour(1 .- log10.(roi4_with_nlin_p),colormap=colormap)
+
+  ╠═╡ =#
 
 # ╔═╡ 8446d2e5-a58f-4b55-8144-801ad4a480bf
 heatmap(ni[:,:,45,50])
@@ -326,7 +282,12 @@ end
 		evts.response_time_centered = evts.response_time_centered ./ std(evts.response_time_centered) # normalize
 
 		# modelfit
-		basis = Unfold.hrfbasis(TR;name="test")
+		#basis = Unfold.hrfbasis(TR;name="test")
+		
+		#gpu_solver =(x, y) -> Unfold.solver_predefined(x, y; solver=:qr)
+		#m = Unfold.fit(UnfoldModel, designDict, evts, cu(dat), solver = gpu_solver)
+		basis = Unfold.firbasis((-1,15),1/TR)
+		#@info "meansignal" size(meansignal) typeof(meansignal)
 		m = fit(UnfoldModel,[Any=>(f,basis)],evts,meansignal)
 		return m
 	end
@@ -342,9 +303,9 @@ begin
 		try
 		_ni = do_read_nifti(ix+offsetix)
 		_evts = do_read_tsv(ix+offsetix)
-		m_all_lin[ix] = do_fit(_ni,_evts,@formula(0~1+response_time_centered))
-		m_all_nlin[ix] = do_fit(_ni,_evts,@formula(0~1+spl(response_time_centered,10)))
-			catch
+		m_all_lin[ix] = do_fit(_ni,_evts,@formula(0~1+condition+response_time_centered))
+		m_all_nlin[ix] = do_fit(_ni,_evts,@formula(0~1+condition+ spl(response_time_centered,10)))
+		catch
 			@info "error with subject-ix $(ix+offsetix)"
 		end
 	end
@@ -354,12 +315,12 @@ end
 # ╔═╡ ad3480e1-7510-40c3-9096-0ef2b1108c4a
 begin
 	good_ix = isassigned.(Ref(m_all_lin),1:length(m_all_lin))
-	coefs_lin = extract_coefs(vcat(m_all_lin[good_ix]...), :response_time_centered,"test")
+	coefs_lin = extract_coefs(vcat(m_all_lin[good_ix]...), :response_time_centered,Any)
 p_values_lin =
     mapslices(c -> pvalue(OneSampleHotellingT2Test(c', [0])), coefs_lin, dims = (3, 4)) |>
     x -> dropdims(x, dims = (3, 4));
 
-coefs_nlin = extract_coefs(vcat(m_all_nlin[good_ix]...), :response_time_centered,"test")
+coefs_nlin = extract_coefs(vcat(m_all_nlin[good_ix]...), :response_time_centered,Any)
 p_values_nlin =
     mapslices(c -> pvalue(OneSampleHotellingT2Test(c', [0,0,0,0,0,0,0,0,0])), coefs_nlin, dims = (3, 4)) |>
     x -> dropdims(x, dims = (3, 4));
@@ -370,14 +331,39 @@ end;
 sum(good_ix)
 
 # ╔═╡ b5066378-2d8c-4101-a0ff-8429d6fb811d
-findall(p_values_nlin.>0.05)
+findall(p_values_nlin[:,10].<0.0000006)
+
+# ╔═╡ d593902f-ce7d-4dae-a77d-6028ddc986c9
+findall(p_values_nlin[:,10].<0.05/(100*24))
+
+# ╔═╡ 52418d10-6aa2-4ae5-a55c-21a596520e06
+let
+	f = Figure()
+
+	heatmap(f[1,1],p_values_lin',colorscale=log10,colorrange=[1,0.000001])
+	heatmap(f[1,2],p_values_lin'.<0.05/(100*24))
+	heatmap(f[2,1],p_values_nlin',colorscale=log10,colorrange=[1,0.000001])
+	heatmap(f[2,2],p_values_nlin'.<0.05/(100*24))
+	f
+end
 
 # ╔═╡ fe91ed09-b02a-46ad-8201-bbdb3871584e
 let
 f = Figure()
 	ax = Axis(f[1,1],yscale=log10)
-		h = plot!(ax,(p_values_lin[:,1]),label="linear")
-h2 = plot!(ax,(p_values_nlin[:,1]),label="non-linear",)
+		h = plot!(ax,(p_values_lin[:,9]),label="linear")
+h2 = plot!(ax,(p_values_nlin[:,9]),label="non-linear",)
+	hlines!([0.05,0.05/100],color=:black,linestyle=:dash)
+	Legend(f[1,2],ax)
+			  f
+end
+
+# ╔═╡ 934cae17-fbc7-436f-bca5-8a9add83192d
+let
+f = Figure()
+	ax = Axis(f[1,1],yscale=log10)
+		h = series!(ax,p_values_lin,solid_color=:black,label="linear")
+	h2 = series!(ax,p_values_nlin,solid_color=:orange,label="non-linear")
 	hlines!([0.05,0.05/100],color=:black,linestyle=:dash)
 	Legend(f[1,2],ax)
 			  f
@@ -393,76 +379,59 @@ begin
 	roi4_with_lin_p .= NaN
 	for r in 1:100
 		_ix = roi4 .== r
-		roi4_with_nlin_p[_ix] .= p_values_nlin[r]
-		roi4_with_lin_p[_ix] .= p_values_lin[r]
+		roi4_with_nlin_p[_ix] .= p_values_nlin[r,10]
+		roi4_with_lin_p[_ix] .= p_values_lin[r,10]
 
 	end
 end	
 
-# ╔═╡ f9d45bce-626b-4666-8c19-da033ddcc504
-# ╠═╡ disabled = true
-#=╠═╡
-begin
-	colormap = to_colormap(:plasma)
-colormap[1] = RGBAf(0,0,0,0)
-f3,ax3,h3 = volume(1 .- log10.(roi4_with_nlin_p),algorithm=:absorption,colormap=colormap,absorption=50)
-	Colorbar(f3[1,2],h3)
-	f3
-end
-  ╠═╡ =#
-
-# ╔═╡ 2db9cac5-efc8-4955-a42c-ff5e193ad9fc
-#=╠═╡
-contour(1 .- log10.(roi4_with_nlin_p),colormap=colormap)
-
-  ╠═╡ =#
-
 # ╔═╡ 9099f03a-aa0d-41c6-a930-d42f94610b32
-sortperm(p_values_nlin,dims=1)[1:10]
+sortperm(p_values_nlin[:,10])
+
+# ╔═╡ e8ea0f12-c283-4f32-b8ee-2df2279a74e3
+minimum(p_values_nlin)
+
+# ╔═╡ cb2949e8-5c35-4f22-bc72-d51e737a049a
+size(p_values_nlin)
 
 # ╔═╡ e2ecc519-a871-4c27-809f-763e64c0cbce
 p_values_lin[20]
 
-# ╔═╡ c0caa903-3a1c-45b3-81be-ca58ba309a1f
-# ╠═╡ show_logs = false
+# ╔═╡ a66402c0-55cb-439d-bac3-2969a852d2cd
+_effects_all = map(x->effects(Dict(:response_time_centered=>-1:0.2:2),x)|>x->subset(x,:channel => x->x.==18),m_all_nlin[good_ix]) #18
+
+#20
+
+#23
+
+#74
+
+
+# ╔═╡ 57fcbd56-e051-410c-ae5d-a9dfb05a9d19
 begin
-		x_rt = -1:0.1:2
-	roi_ix = 25#23#17,18,20,23
-
-	function my_eval_spl(roi_ix)
-	tmp = eval_spl.(Ref(x_rt),m_all_nlin[good_ix],map(x->coef(x)[roi_ix,2:end],m_all_nlin[good_ix]));
-	tmp = map((x,m)->coef(m)[roi_ix,1].+x,tmp,m_all_nlin[good_ix])
-	tmp_centered = map(x->x.-mean(skipmissing(x)),tmp)
-	
-		return tmp,winsor.(collect.(skipmissing.(eachrow(hcat(tmp...))));prop=0.1)
-	end
-	tmp,eval_spline = my_eval_spl(roi_ix)
-	#eval_spline = map(x->winsor(disallowmissing(x),prop=0.1),eval_spline)
-	#eval_spline_centered = skipmissing.(eachrow(hcat(tmp_centered...)))
-	#1:length(eval_spline),
-	f = Figure()
-	ax = Axis(f[1,1])
-	errorbars!(ax,x_rt,mean.(eval_spline),2 .*std.(eval_spline)./sqrt(50))
-	scatter!(ax,x_rt,mean.(eval_spline))
-	ax2 = Axis(f[2,1]) 
-	lines!.(ax2,tmp,color=(:black,0.3))
-
-	#ax = Axis(f[1,2])
-	#errorbars!(ax,x_rt,mean.(eval_spline_centered),std.(eval_spline_centered)./sqrt(50))
-	#scatter!(ax,x_rt,mean.(eval_spline_centered))
-	#ax2 = Axis(f[2,2]) 
-	#lines!.(ax2,tmp_centered,color=(:black,0.3))
-
-	current_figure()
+_effects_avg = _effects_all[1]
+	_effects_avg.yhat .= mean.(winsor.(collect.(skipmissing.(eachrow(hcat([e.yhat for e in _effects_all]...)))),prop=0.1))
 end
+
+# ╔═╡ 109056bf-e3b4-4021-af03-3ff05643f99c
+plot_erp(_effects_avg,mapping=(;color=:response_time_centered,group=:response_time_centered))
+
+# ╔═╡ ed02ab26-73e4-44c2-99e6-2bebbad19e36
+AoG.data(subset(_effects_avg,:time=>x->x.==5.44))*AoG.mapping(:response_time_centered,:yhat)*AoG.visual(Lines)|>AoG.draw
+
+# ╔═╡ 6f9b2c6b-477b-4593-a2c1-1ccf4dab42f1
+AoG.data(_effects_avg)*AoG.mapping(:response_time_centered,:yhat,color=:time,group=:time=>nonnumeric)*AoG.visual(Lines)|>AoG.draw
+
+# ╔═╡ 8f40901a-d205-43c3-a8ea-eb5d143bf44d
+_effects_avg.time|>unique|>sort
 
 # ╔═╡ 3e897e59-cb21-48af-9002-ab7f35f3bb8b
 let
 	f = Figure()
-	highlightroi = [25,33,82]# sortperm(p_values_nlin,dims=1)[1:3]
-	clim = ((0.000000001,0.01))
+	highlightroi = [70]#[18,20,23] #[25,33,82]# sortperm(p_values_nlin,dims=1)[1:3]
+	clim = ((0.05/(100),0.05))
 	p_dat = (roi4_with_nlin_p)
-	options = (;axis=(;aspect=DataAspect()),colorrange=clim,colorscale=log10,colormap = Reverse(:reds),highclip=(:black,0.2))
+	options = (;axis=(;aspect=DataAspect()),colorrange=clim,colorscale=log10,colormap = Reverse(:reds),highclip=(:black,0.2),lowclip=:blue)
 	h1 = heatmap(f[1,1],nanminimum(p_dat,dims=3)[:,:,1];options...)
 	h2 = heatmap(f[1,2],nanminimum(p_dat,dims=2)[:,1,:];options...)
 	h3 = heatmap(f[2,1],nanminimum(p_dat,dims=1)[1,:,:];options...)
@@ -471,7 +440,7 @@ let
 	hidedecorations!.([h1.axis,h2.axis,h3.axis])
 
 	options = (;levels = 1,linewidth=3)
-	ax_roi = Axis(f[2,2])
+	#ax_roi = Axis(f[2,2])
 	for (ix,roi) = enumerate(highlightroi)
 		c = cgrad(:Accent_4 ,4)[ix]
 		contour!(h1.axis,any(roi4.==roi,dims=3)[:,:,1];color=c,options...)
@@ -479,22 +448,26 @@ let
 		contour!(h3.axis,any(roi4.==roi,dims=1)[1,:,:];color=c,options...)
 
 		
-		_,_eval_spline = my_eval_spl(roi)
+		#_,_eval_spline = my_eval_spl(roi)
 		
-		_m = mean.(_eval_spline)./1000
+		#_m = mean.(_eval_spline)./1000
 		
-		_ci = 1.96 *std.(_eval_spline)./sqrt(50) ./1000
-		band!(ax_roi,x_rt,_m.-_ci,_m.+_ci,color=(c,0.3))
-		_h = lines!(ax_roi,x_rt,_m, color=c,linewidth=3, label=split(atlas_labels[roi,:name],'_')[end-1])
-		translate!(_h,0,0,20)
+		#_ci = 1.96 *std.(_eval_spline)./sqrt(50) ./1000
+		#band!(ax_roi,x_rt,_m.-_ci,_m.+_ci,color=(c,0.3))
+		#_h = lines!(ax_roi,x_rt,_m, color=c,linewidth=3, label=split(atlas_labels[roi,:name],'_')[end-1])
+		#translate!(_h,0,0,20)
 
 	end
-	ax_roi.xlabel = "Normalized, centered RT [σ]"
-	ax_roi.ylabel = "centered activation [a.u]"
-	Legend(f[2,3],ax_roi)
-	hidespines!(ax_roi,:r,:t)
+	plot_erp!(f[2,2:3],_effects_avg,mapping=(;color=:response_time_centered,group=:response_time_centered),axis=(;ylabel="BOLD [a.u]",xlabel="Time [s]"))
+	#ax_roi.xlabel = "Normalized, centered RT [σ]"
+	#ax_roi.ylabel = "centered activation [a.u]"
+	#Legend(f[2,3],ax_roi)
+	#hidespines!(ax_roi,:r,:t)
 f
 end
+
+# ╔═╡ 289154e2-2776-4fb6-9dea-cf0eb8a9f959
+m_all_nlin[1]
 
 # ╔═╡ 291647f2-ddce-4655-9257-62f48c7de316
 let
@@ -530,6 +503,7 @@ heatmap(f[1,1],coef(m)[:,1:23])
 end
 
 # ╔═╡ Cell order:
+# ╠═76f5c899-26a2-4bf3-b390-1f76ccf594d4
 # ╠═769aa72b-3a9d-41cb-bb0b-e6a2414aea01
 # ╠═6e6a5571-28f3-4e58-9118-24d015e8c87a
 # ╠═a6fb95f6-1ed0-480f-afb5-1cbf6ff6bb3c
@@ -558,22 +532,33 @@ end
 # ╠═128b2bc2-3813-49ce-a305-0f1ec6b7bd16
 # ╠═9b465222-edf9-4b05-980c-60466ad0c912
 # ╠═08cad98b-bc35-4b4b-b190-8dab9f11ef95
+# ╠═56df2f62-6600-433b-ab3f-5d4d2949a0a3
 # ╠═d8722b44-c724-4136-a3a6-5911643a34a8
 # ╠═ad3480e1-7510-40c3-9096-0ef2b1108c4a
 # ╠═dc7a3d31-a157-4ce6-bf89-04d1e29cf44e
 # ╠═83cb0942-f457-4924-af7c-dab196cad696
 # ╠═b5066378-2d8c-4101-a0ff-8429d6fb811d
+# ╠═d593902f-ce7d-4dae-a77d-6028ddc986c9
+# ╠═52418d10-6aa2-4ae5-a55c-21a596520e06
+# ╠═59e19266-7cba-47d9-a7f1-3e9776519f9e
 # ╠═fe91ed09-b02a-46ad-8201-bbdb3871584e
 # ╠═934cae17-fbc7-436f-bca5-8a9add83192d
 # ╠═b77e5a08-eec9-4984-a971-68234137b32b
 # ╠═42a76743-97cd-4b01-8203-890218819e9a
 # ╠═2ae16a33-bc68-48d8-ab78-c1e89c3d224b
 # ╠═f9d45bce-626b-4666-8c19-da033ddcc504
-# ╠═43c679a1-af3d-4193-8865-fe65f8c5f7f4
-# ╠═c0caa903-3a1c-45b3-81be-ca58ba309a1f
+# ╠═a66402c0-55cb-439d-bac3-2969a852d2cd
+# ╠═57fcbd56-e051-410c-ae5d-a9dfb05a9d19
+# ╠═109056bf-e3b4-4021-af03-3ff05643f99c
+# ╠═ed02ab26-73e4-44c2-99e6-2bebbad19e36
+# ╠═6f9b2c6b-477b-4593-a2c1-1ccf4dab42f1
+# ╠═8f40901a-d205-43c3-a8ea-eb5d143bf44d
+# ╠═7945f3af-c491-4eee-a461-54d7314ad0b6
+# ╟─289154e2-2776-4fb6-9dea-cf0eb8a9f959
 # ╠═9099f03a-aa0d-41c6-a930-d42f94610b32
-# ╠═f619dbf7-a6b9-45b1-af79-6681aa8d9f67
+# ╠═e8ea0f12-c283-4f32-b8ee-2df2279a74e3
 # ╠═6988ca5e-d677-4a8f-9df3-fada317beadb
+# ╠═cb2949e8-5c35-4f22-bc72-d51e737a049a
 # ╠═3e897e59-cb21-48af-9002-ab7f35f3bb8b
 # ╠═e2ecc519-a871-4c27-809f-763e64c0cbce
 # ╠═0ade7e1f-c3ab-46af-af23-fb4fbe939737
@@ -591,6 +576,7 @@ end
 # ╠═e02a1cd5-fcfc-4018-a1a8-fc201a6adaab
 # ╠═35d789e3-431a-47dd-97a7-ee5edc6515d1
 # ╠═c2692000-49ab-44fb-ac7d-f68f5a043af6
+# ╠═c1c2ee66-bec6-4a6f-ae0f-df168c9ac22a
 # ╠═31a70dd6-03ec-4ca0-8e8e-74de6e05b068
 # ╠═39a0e38b-afe7-45aa-a221-b248a636d214
 # ╠═fb36cd7b-93eb-46fa-8990-5c81f9af4597
